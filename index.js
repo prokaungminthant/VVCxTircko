b393e4a02feb4fd1d9ca0deabaac8af0df94e0f8
namekuji <okosimasu3@gmail.com>
2023-12-14 17:13:22 +0900

    added adblocker /wip

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const {
-  app,
-  log,
-  Menu,
-  BrowserWindow,
-  protocol,
-  ipcMain,
+    app,
+    log,
+    Menu,
+    BrowserWindow,
+    protocol,
+    ipcMain,
 } = require("electron");
 const { autoUpdater } = require("electron-updater");
 const localShortcut = require("electron-localshortcut");
 const path = require("path");
 const { clearTimeout } = require("timers");
+const { ElectronBlocker } = require('@cliqz/adblocker-electron');
+const fetch = require('cross-fetch'); // required 'fetch'const
 
 let gameWin = null;
 let splashWin = null;
@@ -17,144 +19,151 @@ let splashWin = null;
 //Electron@9を使用するため、脆弱性の対策
 delete require("electron").nativeImage.createThumbnailFromPath;
 if (!app.requestSingleInstanceLock()) {
-  log.error(
-    "Other process(es) are already existing. Quit. If you can't see the window, please kill all task(s)."
-  );
-  app.exit();
+    log.error(
+        "Other process(es) are already existing. Quit. If you can't see the window, please kill all task(s)."
+    );
+    app.exit();
 }
+
+
+
+
 
 // ビルドしてなくてもしてるように見せかける
 Object.defineProperty(app, "isPackaged", {
-  get() {
-    return true;
-  },
+    get() {
+        return true;
+    },
 });
 
 // vvc://から始まるプロトコルの実装。ローカルファイルにアクセスしていろいろできるようにする
 protocol.registerSchemesAsPrivileged([
-  {
-    scheme: "vvc",
-    privileges: {
-      secure: true,
-      corsEnabled: true,
+    {
+        scheme: "vvc",
+        privileges: {
+            secure: true,
+            corsEnabled: true,
+        },
     },
-  },
 ]);
 
 function createSplash() {
-  splashWin = new BrowserWindow({
-    show: false,
-    frame: false,
-    width: 600,
-    height: 300,
-    resizable: false,
-    transparent: true,
-    alwaysOnTop: true,
-    webPreferences: {
-      preload: path.join(__dirname, "splashWin/preload.js"),
-    },
-  });
-  const update = async () => {
-    let updateCheck = null;
-    autoUpdater.on("checking-for-update", () => {
-      splashWin.webContents.send("status", "Checking for updates...");
-      updateCheck = setTimeout(() => {
-        splashWin.webContents.send("status", "Update check error!");
-        setTimeout(() => {
-          createWindow();
-        }, 1000);
-      }, 15000);
+    splashWin = new BrowserWindow({
+        show: false,
+        frame: false,
+        width: 600,
+        height: 300,
+        resizable: false,
+        transparent: true,
+        alwaysOnTop: true,
+        webPreferences: {
+            preload: path.join(__dirname, "splashWin/preload.js"),
+        },
     });
-    autoUpdater.on("update-available", (i) => {
-      if (updateCheck) clearTimeout(updateCheck);
-      splashWin.webContents.send("status", `Found new verison v${i.version}`);
-    });
-    autoUpdater.on("update-not-available", () => {
-      if (updateCheck) clearTimeout(updateCheck);
-      splashWin.webContents.send("status", "You using latest version.");
-      setTimeout(() => {
-        createWindow();
-      }, 1000);
-    });
+    const update = async () => {
+        let updateCheck = null;
+        autoUpdater.on("checking-for-update", () => {
+            splashWin.webContents.send("status", "Checking for updates...");
+            updateCheck = setTimeout(() => {
+                splashWin.webContents.send("status", "Update check error!");
+                setTimeout(() => {
+                    createWindow();
+                }, 1000);
+            }, 15000);
+        });
+        autoUpdater.on("update-available", (i) => {
+            if (updateCheck) clearTimeout(updateCheck);
+            splashWin.webContents.send("status", `Found new verison v${i.version}`);
+        });
+        autoUpdater.on("update-not-available", () => {
+            if (updateCheck) clearTimeout(updateCheck);
+            splashWin.webContents.send("status", "You using latest version.");
+            setTimeout(() => {
+                createWindow();
+            }, 1000);
+        });
 
-    autoUpdater.on("error", (e) => {
-      if (updateCheck) clearTimeout(updateCheck);
-      splashWin.webContents.send("status", "Error!" + e.name);
-      setTimeout(() => {
-        createWindow();
-      }, 1000);
-    });
-    autoUpdater.on("download-progress", (i) => {
-      if (updateCheck) clearTimeout(updateCheck);
-      splashWin.webContents.send("status", "Downloading new version...");
-    });
-    autoUpdater.on("update-downloaded", (i) => {
-      if (updateCheck) clearTimeout(updateCheck);
-      splashWin.webContents.send("status", "Update downloaded");
-      setTimeout(() => {
-        autoUpdater.quitAndInstall();
-      }, 1000);
-    });
-    autoUpdater.autoDownload = "download";
-    autoUpdater.allowPrerelease = false;
-    autoUpdater.checkForUpdates();
-  };
-  // スプラッシュ用のHTMLを表示
-  splashWin.loadFile(path.join(__dirname, "splashWin/splash.html"));
+        autoUpdater.on("error", (e) => {
+            if (updateCheck) clearTimeout(updateCheck);
+            splashWin.webContents.send("status", "Error!" + e.name);
+            setTimeout(() => {
+                createWindow();
+            }, 1000);
+        });
+        autoUpdater.on("download-progress", (i) => {
+            if (updateCheck) clearTimeout(updateCheck);
+            splashWin.webContents.send("status", "Downloading new version...");
+        });
+        autoUpdater.on("update-downloaded", (i) => {
+            if (updateCheck) clearTimeout(updateCheck);
+            splashWin.webContents.send("status", "Update downloaded");
+            setTimeout(() => {
+                autoUpdater.quitAndInstall();
+            }, 1000);
+        });
+        autoUpdater.autoDownload = "download";
+        autoUpdater.allowPrerelease = false;
+        autoUpdater.checkForUpdates();
+    };
+    // スプラッシュ用のHTMLを表示
+    splashWin.loadFile(path.join(__dirname, "splashWin/splash.html"));
 
-  // 準備が整ったら表示
-  splashWin.webContents.on("did-finish-load", () => {
-    splashWin.show();
-    update();
-  });
+    // 準備が整ったら表示
+    splashWin.webContents.on("did-finish-load", () => {
+        splashWin.show();
+        update();
+    });
 }
 
 //ウィンドウの作成
 function createWindow() {
-  gameWin = new BrowserWindow({
-    width: 1920,
-    height: 1080,
-    fullscreen: true,
-    show: false,
-    title: "Vanded Voxiom Client",
-    icon: path.join(__dirname, "icon.ico"),
-    webPreferences: {
-      preload: path.join(__dirname, "./preload/game.js"),
-      enableRemoteModule: true,
-      experimentalFeatures: true,
-      enableHardwareAcceleration: true,
-      contextIsolation: false,
-    },
-  });
-  // ウインドウタイトルを固定する
-  gameWin.setTitle("Vanded Voxiom Client");
+    gameWin = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        fullscreen: true,
+        show: false,
+        title: "Vanded Voxiom Client",
+        icon: path.join(__dirname, "icon.ico"),
+        webPreferences: {
+            preload: path.join(__dirname, "./preload/game.js"),
+            enableRemoteModule: true,
+            experimentalFeatures: true,
+            enableHardwareAcceleration: true,
+            contextIsolation: false,
+        },
+    });
+    ElectronBlocker.fromPrebuiltAdsAndTracking(fetch).then((blocker) => {
+        blocker.enableBlockingInSession(session.defaultSession);
+    });
+    // ウインドウタイトルを固定する
+    gameWin.setTitle("Vanded Voxiom Client");
 
-  Menu.setApplicationMenu(null);
-  //ESCの実装。Preloadで受け取り
-  localShortcut.register(gameWin, "Esc", () => {
-    gameWin.webContents.send("ESC");
-  });
-  localShortcut.register(gameWin, "F5", () => {
-    gameWin.reload();
-  });
-  localShortcut.register(gameWin, "F8", () => {
-    gameWin.loadURL("https://voxiom.io");
-    // gameWin.reload();
-    gameWin.webContents.loadURL("https://voxiom.io/experimental");
-  });
-  localShortcut.register(gameWin, "F11", () => {
-    const isFS = gameWin.isFullScreen();
-    gameWin.setFullScreen(!isFS);
-  });
-  localShortcut.register(gameWin, "F12", () => {
-    gameWin.webContents.openDevTools();
-  });
-  gameWin.webContents.loadURL("https://voxiom.io");
+    Menu.setApplicationMenu(null);
+    //ESCの実装。Preloadで受け取り
+    localShortcut.register(gameWin, "Esc", () => {
+        gameWin.webContents.send("ESC");
+    });
+    localShortcut.register(gameWin, "F5", () => {
+        gameWin.reload();
+    });
+    localShortcut.register(gameWin, "F8", () => {
+        gameWin.loadURL("https://voxiom.io");
+        // gameWin.reload();
+        gameWin.webContents.loadURL("https://voxiom.io/experimental");
+    });
+    localShortcut.register(gameWin, "F11", () => {
+        const isFS = gameWin.isFullScreen();
+        gameWin.setFullScreen(!isFS);
+    });
+    localShortcut.register(gameWin, "F12", () => {
+        gameWin.webContents.openDevTools();
+    });
+    gameWin.webContents.loadURL("https://voxiom.io");
 
-  gameWin.once("ready-to-show", () => {
-    splashWin.destroy();
-    gameWin.show();
-  });
+    gameWin.once("ready-to-show", () => {
+        splashWin.destroy();
+        gameWin.show();
+    });
 }
 
 //flags
@@ -168,17 +177,17 @@ app.commandLine.appendSwitch("enable-pointer-lock-options");
 
 //app
 app.whenReady().then(() => {
-  // スプラッシュを最初に表示
-  createSplash();
+    // スプラッシュを最初に表示
+    createSplash();
 });
 
 app.on("ready", () => {
-  protocol.registerFileProtocol("vvc", (request, callback) =>
-    callback(decodeURI(request.url.replace(/^vvc:\//, "")))
-  );
+    protocol.registerFileProtocol("vvc", (request, callback) =>
+        callback(decodeURI(request.url.replace(/^vvc:\//, "")))
+    );
 });
 
 ipcMain.handle("appVer", () => {
-  const version = app.getVersion();
-  return version;
+    const version = app.getVersion();
+    return version;
 });
--- preload/game.js
+++ preload/game.js
@@ -2,66 +2,67 @@ const { ipcRenderer, ipcMain } = require("electron");
 const account = require("../util/account");
 const path = require("path");
 let ver = "";
+
+
 ipcRenderer.invoke("appVer").then((version) => {
-  ver = version;
+    ver = version;
 });
 // // //ESCの入力を受け取り
 // //100ms秒のDelayを入れるとメニューが消えなくて済む。機体によっても差が出るかもしれないから要検証
-// ipcRenderer.on("ESC", () => {
-//   setTimeout(() => {
-//     document.activeElement.blur();
-//     document.exitPointerLock();
+ipcRenderer.on("ESC", () => {
+    setTimeout(() => {
+        document.activeElement.blur();
+        document.exitPointerLock();
 
-//   }, 100);
-// });
+    }, 70);
+});
 ipcRenderer.on("F8", () => {
-  location.href = "https://voxiom.io/";
+    location.href = "https://voxiom.io/";
 });
 
 //右下のロゴ
 function vvcLogo() {
-  let tempDom4 = `<div id="clientLogo">Vanced Voxiom Client v${ver}</div>`;
-  document.body.insertAdjacentHTML("beforeend", tempDom4);
+    let tempDom4 = `<div id="clientLogo">Vanced Voxiom Client v${ver}</div>`;
+    document.body.insertAdjacentHTML("beforeend", tempDom4);
 }
 
 function newPage() {
-  //VVC用の最低限のCSSをinject
-  let cssIn = document.createElement("link");
-  cssIn.setAttribute("rel", "stylesheet");
-  cssIn.setAttribute("href", `vvc://${path.join(__dirname, "/css/vvc.css")}`);
-  document.head.appendChild(cssIn);
-  console.log("css injected");
-  if (!document.getElementById("clientLogo")) {
-    vvcLogo();
-  }
+    //VVC用の最低限のCSSをinject
+    let cssIn = document.createElement("link");
+    cssIn.setAttribute("rel", "stylesheet");
+    cssIn.setAttribute("href", `vvc://${path.join(__dirname, "/css/vvc.css")}`);
+    document.head.appendChild(cssIn);
+    console.log("css injected");
+    if (!document.getElementById("clientLogo")) {
+        vvcLogo();
+    }
 
-  //VVCの設定用のJSをinject
-  let settingJsIn = document.createElement("script");
-  settingJsIn.setAttribute("type", "text/javascript");
-  settingJsIn.setAttribute(
-    "src",
-    `vvc://${path.join(__dirname, "../util/settingMenu.js")}`
-  );
-  document.body.appendChild(settingJsIn);
-  console.log("Setting.js injected");
+    //VVCの設定用のJSをinject
+    if (!document.getElementById("settingJS")) {
+        let settingJsIn = document.createElement("script");
+        settingJsIn.setAttribute("type", "text/javascript");
+        settingJsIn.setAttribute("id", "settingJS")
+        settingJsIn.setAttribute(
+            "src",
+            `vvc://${path.join(__dirname, "../util/settingMenu.js")}`
+        );
+        document.body.appendChild(settingJsIn);
+        console.log("Setting.js injected");
+    }
 
-  //ページの変遷を検知してページごとに動作を分岐
-  let url = location.href;
-  if (url === "https://voxiom.io/account") {
-    account.pageload();
-  } else if (url === "https://voxiom.io/#google_vignette") {
-    location.href = "https://voxiom.io/experimental";
-  } else if (url === "https://voxiom.io/experimental#google_vignette") {
-    location.href = "https://voxiom.io/";
-  }
+    //ページの変遷を検知してページごとに動作を分岐
+    let url = location.href;
+    if (url === "https://voxiom.io/account") {
+        account.pageload();
+    }
 }
 
 // 0.1秒ごとにURLを確認してページの変遷を検知するインターバル関数
 let curUrl = "";
 setInterval(() => {
-  let newUrl = window.location.href;
-  if (curUrl !== newUrl) {
-    newPage();
-    curUrl = newUrl;
-  }
+    let newUrl = window.location.href;
+    if (curUrl !== newUrl) {
+        newPage();
+        curUrl = newUrl;
+    }
 }, 100);
